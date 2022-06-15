410584ec2ff6a01c0d81fd708d2489827ad9b994
gitthathonor <jeong.suyoung92@gmail.com>
2022-06-15 22:56:59 +0900

    혼공자 13장 컬렉션 프레임워크 - Stack&Queue

---
+++ JavaTest/src/chapter13/sec02/exam01/StackExample.java
@@ -0,0 +1,20 @@
+package chapter13.sec02.exam01;
+
+import java.util.Stack;
+
+public class StackExample {
+	public static void main(String[] args) {
+		Stack<Coin> coinBox = new Stack<Coin>();
+		
+		// 동전을 투입
+		coinBox.push(new Coin(100));
+		coinBox.push(new Coin(50));
+		coinBox.push(new Coin(500));
+		coinBox.push(new Coin(10));
+	
+		while(!coinBox.isEmpty()) {
+			Coin coin = coinBox.pop();
+			System.out.println("꺼내온 동전 : " + coin.getValue() + "원");
+		}
+	}
+}
